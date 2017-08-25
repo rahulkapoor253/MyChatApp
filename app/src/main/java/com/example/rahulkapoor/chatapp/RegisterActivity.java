b16375e816f8febc0cb6c74b3e37c7481b7dda92
Rahul Kapoor <rahul253801@gmail.com>
2017-08-25 21:00:00 +0530

    real time database added

--- app/src/main/java/com/example/rahulkapoor/chatapp/RegisterActivity.java
+++ app/src/main/java/com/example/rahulkapoor/chatapp/RegisterActivity.java
@@ -16,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,6 +30,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private String mPassword, mUserName, mEmail;
     private FirebaseAuth mAuth;
     private ProgressDialog mProgressDialog;
+    private String uID;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,17 +105,53 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             @Override
             public void onComplete(@NonNull final Task<AuthResult> task) {
                 //it tells us about the signin failure or success;
-                if (!task.isSuccessful()) {
+                if (task.isSuccessful()) {
+
+                    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                    if (currentUser != null) {
+                        uID = currentUser.getUid();
+                    }
+
+                    //setting up the realtime database;
+                    database = FirebaseDatabase.getInstance();
+                    //root->child object of users -> child with UID ->setting up fields;
+                    myRef = database.getReference().child("Users").child(uID);
+
+                    //we are using thumbnail image and complete image because we dont want user to load full image;
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("name", mUserName);
+                    map.put("image", "default");
+                    map.put("thumb_image", "default");
+                    map.put("status", "Hi there! I am using Chat App");
+
+                    //to check whether my task has been completed/ not;
+                    myRef.setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull final Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                //    to dismiss the progress bar once the task is successful;
+                                mProgressDialog.dismiss();
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                                finish();
+
+                            } else {
+                                //to hide the progress bar when registering user fails;
+                                mProgressDialog.hide();
+                                Toast.makeText(RegisterActivity.this, "Fail to Register User, Try again!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+                } else {
                     //to hide the progress bar when registering user fails;
                     mProgressDialog.hide();
                     Toast.makeText(RegisterActivity.this, "Fail to Register User, Try again!", Toast.LENGTH_SHORT).show();
-                } else {
-                    //to dismiss the progress bar once the task is successful;
-                    mProgressDialog.dismiss();
-                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    finish();
+
+
                 }
 
 
